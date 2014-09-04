46f3fcd8c2b9cadd367103e3e6f0bb3e48b587fe
Acharya <macharya@cslab02.cs.skidmore.edu>
2014-09-03 20:06:20 -0400

    Initial commit

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author macharya
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
