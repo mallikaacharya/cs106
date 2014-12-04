6448aee13c7a04a8ae0b308001824fa19d8d825a
mallikaacharya <macharya@skidmore.edu>
2014-12-03 20:37:11 -0500

    edits

--- FinalProject/src/Images.java
+++ FinalProject/src/Images.java
@@ -4,8 +4,25 @@
 
 /**
  * @author mallikaacharya
- *
+ *This class uses the scores from the quiz to generate the face. 
  */
 public class Images {
 
 }
+
+
+
+//}
+//}
+//This will probably be a different method
+//public static void readFile() throws FileNotFoundException{
+//Scanner fileReader = new Scanner(new File(FeatureImages.txt));
+//while(fileReader.hasNext()) {
+//String FileName = fileReader.next();
+//String FeatureType = fileReader.next();
+//String ImageType = fileReader.next(".jpg");
+//String FileLocation = fileReader.next("Documents/FinalJPG/"+FileName+ImageType);
+
+//fi[i] = new FeatImg(FileName, FeatureType, ImageType, FileLocation);		
+//}
+//}
